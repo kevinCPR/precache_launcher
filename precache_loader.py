bad35d375c3010d469fbf4e08c10faa311abf18f
knguyen-CPR <kevin@cpr-minnesota.com>
2020-02-21 15:24:07 -0600

    fixed port

--- precache_loader.py
+++ precache_loader.py
@@ -12,12 +12,12 @@ for i in mobileDevices:
     model = i.split(':')[0]
     friendlyName = i.split(':')[1][1:]
     if "iPhone" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPhone/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49237 -i " + model + " -o ~/precache/IPSWs/iPhone/"
     if "iPod" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPod/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49237 -i " + model + " -o ~/precache/IPSWs/iPod/"
     if "iPad" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPad/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49237 -i " + model + " -o ~/precache/IPSWs/iPad/"
     if "Watch" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/Watch/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49237 -i " + model + " -o ~/precache/IPSWs/Watch/"
     print(friendlyName)
     os.system(command)
