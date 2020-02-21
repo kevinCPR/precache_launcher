ef244c3f6e8979970c79328e091143c1e1c085a3
knguyen-CPR <kevin@cpr-minnesota.com>
2020-02-21 14:31:12 -0600

    edit path for commands

--- precache_loader.py
+++ precache_loader.py
@@ -11,12 +11,12 @@ for i in mobileDevices:
     model = i.split(':')[0]
     friendlyName = i.split(':')[1][1:]
     if "iPhone" in model:
-        command = "./precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/iPhone/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPhone/"
     if "iPod" in model:
-        command = "./precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/iPod/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPod/"
     if "iPad" in model:
-        command = "./precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/iPad/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPad/"
     if "Watch" in model:
-        command = "./precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/Watch/"
+        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/Watch/"
     print(friendlyName)
     os.system(command)
