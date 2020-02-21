2133f6ffcc8c718e61c1398764d729322ff10938
knguyen-CPR <kevin@cpr-minnesota.com>
2020-02-21 14:43:29 -0600

    edit path for commands

--- precache_loader.py
+++ precache_loader.py
@@ -1,6 +1,7 @@
 import os
-
-with open('~/precache/precache_launcher/Apple_mobile_device_types.txt') as f:
+from os.path import expanduser
+home = expanduser("~")
+with open('home/precache/precache_launcher/Apple_mobile_device_types.txt') as f:
     mobileDevices = f.read().splitlines()
 
 i = 1
@@ -11,12 +12,12 @@ for i in mobileDevices:
     model = i.split(':')[0]
     friendlyName = i.split(':')[1][1:]
     if "iPhone" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPhone/"
+        command = "home/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o home/precache/IPSWs/iPhone/"
     if "iPod" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPod/"
+        command = "home/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o home/precache/IPSWs/iPod/"
     if "iPad" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/iPad/"
+        command = "home/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o home/precache/IPSWs/iPad/"
     if "Watch" in model:
-        command = "~/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o ~/precache/IPSWs/Watch/"
+        command = "home/precache/precache/precache.py -cs http://localhost:49269 -i " + model + " -o home/precache/IPSWs/Watch/"
     print(friendlyName)
     os.system(command)
